8e6cc7a4584548763239dea58bdfae752263b0b3
juan <juan00xt@g,ail.com>
2014-11-29 18:37:44 -0500

    fix

--- app/scripts/controllers/controller.js
+++ app/scripts/controllers/controller.js
@@ -8,19 +8,19 @@
  * Controller of the apps
  */
 
-  var portapp = angular.module( 'apps' , ['ngRoute'] )
+var portapp = angular.module( 'apps' , ['ngRoute'] )
           
-         portapp.config(['$routeProvider',
-              function($routeProvider) {
-                $routeProvider.
-                  when('/skills', {
-                    templateUrl: 'views/skills.html',
-                  }).
-                  when('/portfolio', {
-                    templateUrl: 'views/portfolio.html',
-                  }).
-                  otherwise({
-                    redirectTo: '/',
-                      templateUrl: 'views/start.html',
-                  });
-              }]);
+portapp.config(['$routeProvider',
+   function($routeProvider) {
+        $routeProvider.
+           when('/skills', {
+             templateUrl: 'views/skills.html',
+           }).
+           when('/portfolio', {
+              templateUrl: 'views/portfolio.html',
+            }).
+            otherwise({
+              redirectTo: '/',
+              templateUrl: 'views/start.html',
+         });
+    }]);

3b2f6fa968b5eeec6441e9d7d684bb5365a672c5
juan <juan00xt@g,ail.com>
2014-12-08 19:51:51 -0500

    some improvements

--- app/scripts/controllers/controller.js
+++ app/scripts/controllers/controller.js
@@ -10,50 +10,29 @@
 
 
   
-  var AppStart = angular.module('apps', ['ngRoute'])
-    
-    AppStart.controller( 'skillList1', ['$scope', function($scope){
+var AppStart = angular.module('apps', ['ngRoute'])
 
-       $scope.skills = [
-            { skill: "Html5", perct: "5" },
-            { skill: "Css3", perct: "10" },
-            { skill: "Jquery", perct: "20" },
-            { skill: "Jquery UI", perct: "40" },
-            { skill: "Angular JS", perct: "50" },
-            { skill: "Sql", perct: "40" },
-            { skill: "php", perct: "40" },
-            { skill: "Responsive Design", perct: "30" }
+        AppStart.controller('skillList', function ($scope, $http){
 
-
-        ]
-
-        $scope.skills2 = [
-            { skill: "Bootstrap 3", perct: "30" },
-            { skill: "Wordpress", perct: "15" },
-            { skill: "SEO", perct: "40" },
-            { skill: "User Experience", perct: "20" }
-
-        ]
-
-        $scope.skills3 = [
-            { skill: "Less", perct: "30" },
-            { skill: "Bower", perct: "40" },
-            { skill: "GruntJS", perct: "50" },
-            { skill: "GitHub", perct: "50" }
+              $http.get('data/skill.json').success(function(data) {
+                $scope.skills = data; 
+              });
+              $http.get('data/skill2.json').success(function(data) {
+                $scope.skills2 = data; 
+              });
+              $http.get('data/skill3.json').success(function(data) {
+                $scope.skills3 = data; 
+              });
+            });
 
 
-        ] 
-
-    }] )
-
-         AppStart.controller('PortfolioController', function ($scope, $http){
+        AppStart.controller('PortfolioController', function ($scope, $http){
               $http.get('data/portfolio.json').success(function(data) {
                 $scope.portlist = data; 
               });
             });
 
-
-         AppStart.config(['$routeProvider', function($routeProvider) {
+        AppStart.config(['$routeProvider', function($routeProvider) {
                 $routeProvider.
                 when('/portfolio', {
                     templateUrl: 'views/portfolio.html',

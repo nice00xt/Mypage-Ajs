111d10c43515535cc9057f893b0d02d16b385d43
juan <juan00xt@g,ail.com>
2015-01-26 16:54:01 -0500

    delete

---
+++ scripts/controllers/controller.js
@@ -0,0 +1,83 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name apps.controller:controller
+ * @description
+ * # controller
+ * Controller of the apps
+ */
+
+
+  
+var AppStart = angular.module('apps', ['ngRoute', 'ngAnimate'])
+
+AppStart.config(['$routeProvider', function($routeProvider) {
+                $routeProvider.
+                when('/portfolio', {
+                    templateUrl: 'views/portfolio.html',
+                    controller: 'PortfolioController'
+
+                  }).
+                  when('/skills', {
+                      templateUrl: 'views/skills.html',
+                      controller: 'skillList'
+
+                    }).
+                  when('/portfolio/:details', {
+                    templateUrl: 'views/port-view.html',
+                    controller: 'PortfolioDetails'
+
+                  }).
+                  otherwise({
+                    redirectTo: '/',
+                      templateUrl: 'views/start.html',
+                  });
+              }]);
+
+//animate views ===================================================
+        AppStart.controller('PortfolioController', function($scope) {
+            $scope.pageClass = 'page-porfolio';
+        });
+
+        AppStart.controller('skillList', function($scope) {
+            $scope.pageClass = 'page-skills';
+        });
+
+
+//skills json file
+        AppStart.controller('skillList', function ($scope, $http){
+              $http.get('data/skill.json').success(function(data) {
+                $scope.skills = data; 
+              });
+              $http.get('data/skill2.json').success(function(data) {
+                $scope.skills2 = data; 
+              });
+              $http.get('data/skill3.json').success(function(data) {
+                $scope.skills3 = data; 
+              });
+            });
+
+//portolio json file
+        AppStart.controller('PortfolioController', function ($scope, $http){
+              $http.get('data/portfolio.json').success(function(data) {
+                $scope.portlist = data; 
+              });
+            });
+
+//route
+        AppStart.controller('PortfolioDetails', function ($scope, $routeParams, $http){
+        $scope.title = $routeParams.details;
+
+
+       $http.get('data/portfolio.json').success(function(data) {
+
+       $scope.prt = data.filter(function(entry){
+            return entry.title === $scope.title;
+          })[0];
+        });
+      });
+
+
+       
+           
---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+/*
+
+$(document).ready(function(){
+	$('.btn-vr').click(function(){
+		$('.overlay-box').fadeIn('fast');
+	});
+
+	$('.closeup').click(function(){
+		$('.overlay-box').fadeOut('fast');
+	});
+});
+
+*/

9908761f8c92224737efd530eca8b740e9df3222
juan <juan00xt@g,ail.com>
2015-02-18 18:38:23 -0500

    add change

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,3 @@
-/*
 
-$(document).ready(function(){
-	$('.btn-vr').click(function(){
-		$('.overlay-box').fadeIn('fast');
-	});
 
-	$('.closeup').click(function(){
-		$('.overlay-box').fadeOut('fast');
-	});
-});
 
-*/
